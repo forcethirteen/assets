4dbd6a0e54c6add844ddc324186f39727288e892
Jason Mudd <jasonmudd@iinet.net.au>
2022-11-30 15:14:25 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,11 +1,4 @@
-var yt = 0;
-var ad = "<iframe data-aa='1922114' src='//acceptable.a-ads.com/1922114' style='border:0; padding:0; width:100%; height:100%; overflow:hidden; background-color: transparent;'></iframe>";
 if(window.location.href.indexOf('force-13.com/cyclone-tracker') != -1){
-    if(document.getElementById('commentform')){
-        document.getElementById('commentform').innerHTML += '<span style="display:block;width:100%;margin:0 auto;text-align:center;color:#cccccc;">ADVERTISEMENT</span>' +
-            '<div style="border: 1px solid #dddddd;width: 746px;min-height: 95px;display: block;margin: 0 auto 25px auto;padding: 8px 6px 2px 6px;">' +
-            ad + '</div>';
-    }
     if(document.getElementById('layer01')){
         document.getElementById('layer01').style.display = 'block';
         document.getElementById('layer01').style.background = 'url(\'https://vortex.plymouth.edu/mapwall/goes/irg/gbsat.24.png\')';
@@ -108,32 +101,3 @@ if(window.location.href.indexOf('force-13.com/cyclone-tracker') != -1){
         document.querySelector('.entry-content').innerHTML = document.querySelector('.entry-content').innerHTML.replace(/gif"/g,'gif?' + satInfoMonth + "_" + satInfoDay + "_" + satInfoYear + "-" + satInfoHour + '00Z"');
     }
 }
-if(document.querySelector('.entry-content') !== null){
-    var width = document.querySelector('.entry-content').offsetWidth;
-    if(width >= 800){
-        document.querySelector('.entry-content').innerHTML = document.querySelector('.entry-content').innerHTML.replace('</iframe>','</iframe><div id="yt-ad" style="display:block;width:100%;margin:0 auto;"></div>');
-    }else{
-        document.querySelector('.entry-content').innerHTML = document.querySelector('.entry-content').innerHTML.replace('<iframe ','<div class="video-container"><iframe ').replace('</iframe>','</iframe></div><div id="yt-ad" style="display:block;width:100%;margin:0 auto;"></div>');
-    }
-}
-var monitor = setInterval(function(){
-    var elem = document.activeElement;
-    if(elem && elem.tagName == 'IFRAME'){
-        clearInterval(monitor);
-        if(yt == 0){
-            yt = 1;
-            if(document.getElementById('yt-ad')){
-                var width = document.querySelector('.entry-content').offsetWidth;
-                if(width >= 800){
-                    document.getElementById('yt-ad').innerHTML = '<br><span style="display:block;width:100%;margin:0 auto;text-align:center;color:#cccccc;">ADVERTISEMENT</span>' +
-                        '<div style="border: 1px solid #dddddd;width: 746px;min-height: 95px;display: block;margin: 0 auto 25px auto;padding: 8px 6px 2px 6px;">' +
-                        ad + '</div>';
-                }else{
-                    document.getElementById('yt-ad').innerHTML = '<br><span style="display:block;width:100%;margin:0 auto;text-align:center;color:#cccccc;">ADVERTISEMENT</span>' +
-                        '<div style="border: 1px solid #dddddd;width: 100%;min-height: 95px;display: block;margin: 0 auto 25px auto;padding: 8px 0 2px 0;">' +
-                        ad + '</div>';
-                }
-            }
-        }
-    }
-}, 100);
